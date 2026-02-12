46b70845faf9a36509fa1bd367c782804c952045
Adis-ore <adisoreoluwa@gmail.com>
2026-02-12 16:55:44 +0100

    Final

--- src/Pages/GettingAround.jsx
+++ src/Pages/GettingAround.jsx
@@ -175,7 +175,7 @@ const GettingAround = () => {
             
             <div className="h-[400px] md:h-[500px]">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.0123456789!2d3.3445678!3d7.1234567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwMDcnMjQuNSJOIDPCsDIwJzQwLjQiRQ!5e0!3m2!1sen!2sng!4v1234567890123!5m2!1sen!2sng"
+                src="https://maps.app.goo.gl/7HfaFm48ysqrTZWa7"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
